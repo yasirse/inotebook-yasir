2edb36da2224193cfde8495d183d73119d7d4e70
yasircs <yasir.arfatcs@gmail.com>
2023-10-19 20:45:14 +0500

    before addnote

--- backend/models/Notes.js
+++ backend/models/Notes.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
-
+const { Schema } = mongoose;
 const NotesSchema = new Schema({
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user1'
+    },
     title:{
-        type:string,
+        type:String,
         required:true
     },
     description:{
-        type:string,
+        type:String,
         required:true,
     },
     tag:{
-        type:string,
+        type:String,
         default:"General"
     },
     date:{
--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -1,13 +1,43 @@
-const express=require('express')
-const Router=express.Router()
+const express = require("express");
+const Router = express.Router();
+const Note = require("../models/Notes");
+var fetchuser = require("../middleware/fetchuser");
 
-Router.get('/',(req,res)=>{
-    obj={
-        notes:'hello',
-        id:34
+Router.get("/fetchallnotes", fetchuser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Some Error occured");
+  }
+});
+
+//
+Router.get(
+  "/addnote",
+  fetchuser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 3 }),
+    body("description", "Enter a valid email").isEmail(),
+    body("password", "Description must be 5 character").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+      // If there agre errors, return Bad request and the errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+      const note = new Note({ title, description, tag, user: req.user.id });
+      const savedNote = await note.save();
+      res.json(savedNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some Error occured");
     }
-    res.json(obj);
-})
+  }
+);
 
-module.exports=Router
-
+module.exports = Router;
